9125e6ba7d92dc0b2a4348629183a868ea860b5a
climacodev <climacodev@gmail.com>
2024-09-17 15:03:34 -0300

    .

---
+++ LoginOauth2/target/classes/import.sql
@@ -0,0 +1,18 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERADOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_product (name) VALUES ('TV');
+INSERT INTO tb_product (name) VALUES ('Computer');
+
+
+select * from tb_role;
+select * from tb_user;
+select * from tb_user_role;
