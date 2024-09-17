3138dd048b24c93bb34a67392595c61b637289c2
climacodev <climacodev@gmail.com>
2024-09-17 18:44:18 -0300

    .

--- LoginOauth2/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ LoginOauth2/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.devsuperior.demo;
 
+import java.net.PasswordAuthentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passawordEnconder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Enconde = " + passawordEnconder.encode("123456"));
+
+		// faz a comparação para ver se a senha digitada é igual a criptografia do hash
+		boolean result = passawordEnconder.matches("123456",
+				"$2a$10$im/cAH50/9vJepApECQsHuRyl6hb5H.9dKqWQErxFF1PSgEP2x3Ua");
+		
+		System.out.println("RESULTADO = " + result);
 	}
 
 }
--- LoginOauth2/src/main/resources/import.sql
+++ LoginOauth2/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$im/cAH50/9vJepApECQsHuRyl6hb5H.9dKqWQErxFF1PSgEP2x3Ua');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$im/cAH50/9vJepApECQsHuRyl6hb5H.9dKqWQErxFF1PSgEP2x3Ua');
 
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERADOR');
